73c062fece8908939d7a086f5c255743b1870d3d
Marcus Leitão <marcusvinicius.leitao@i9xp.com.br>
2020-03-29 13:44:59 -0300

    criação de controllers, metodos get, post e delete

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,16 +1,14 @@
 const express = require("express");
-
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
-
-    console.log(body);
-
-    return response.json({
-        teste: 'teste',
-        alunoe: 'marcus'
-    });
-});
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
+routes.delete('/incidents/:id', IncidentController.delete);
+routes.get('/profile', ProfileController.index);
 
 module.exports = routes;
